31a62f613394424c2c5fe01fe284ad12174427b1
KESAVAPRABHA <kesavaprabhal.22cse@kongu.edu>
2025-06-22 12:27:21 +0530

    Adding Week1(Design Pattern) Contents

---
+++ Week1/Design Pattern and Priciples/Exercise1_Implementing_the_singleton_pattern/SingletonPatternExample.java
@@ -0,0 +1,30 @@
+public class SingletonPatternExample {
+    static class Logger {
+        private static Logger instance;
+
+        private Logger() {
+            System.out.println("Logger instance created");
+        }
+
+        public static Logger getInstance() {
+            if (instance == null) {
+                instance = new Logger();
+            }
+            return instance;
+        }
+
+        public void log(String message) {
+            System.out.println("Log: " + message);
+        }
+    }
+
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("This is the first log message.");
+        logger2.log("This is the second log message.");
+
+        System.out.println("logger1 and logger2 are the same instance: " + (logger1 == logger2));
+    }
+}
---
+++ Week1/Design Pattern and Priciples/Exercise2_Factorymethodpattern/FactoryMethodPatternExample.java
@@ -0,0 +1,62 @@
+public class FactoryMethodPatternExample {
+
+    // Document interface
+    interface Document {
+        void open();
+    }
+
+    static class WordDocument implements Document {
+        public void open() {
+            System.out.println("Opening Word document");
+        }
+    }
+
+    static class PdfDocument implements Document {
+        public void open() {
+            System.out.println("Opening PDF document");
+        }
+    }
+
+    static class ExcelDocument implements Document {
+        public void open() {
+            System.out.println("Opening Excel document");
+        }
+    }
+
+    // Abstract Factory
+    static abstract class DocumentFactory {
+        public abstract Document createDocument();
+    }
+
+    static class WordFactory extends DocumentFactory {
+        public Document createDocument() {
+            return new WordDocument();
+        }
+    }
+
+    static class PdfFactory extends DocumentFactory {
+        public Document createDocument() {
+            return new PdfDocument();
+        }
+    }
+
+    static class ExcelFactory extends DocumentFactory {
+        public Document createDocument() {
+            return new ExcelDocument();
+        }
+    }
+
+    public static void main(String[] args) {
+        DocumentFactory wordFactory = new WordFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+
+        DocumentFactory excelFactory = new ExcelFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+    }
+}
